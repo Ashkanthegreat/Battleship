d2fba105b99f4670cc6dac5f08e6ae2e3cdf3004
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-03-24 11:20:57 -0600

    testing for ship health

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -15,8 +15,8 @@ class ShipTest < Minitest::Test
     assert_equal "Cruiser", @cruiser.name
   end
 
-  def test_can_retrieve_ship_length
+  def test_for_ship_attributes
     assert_equal 3, @cruiser.length
-
+    assert_equal 3, @cruiser.health
   end
 end
