399b9d4195ca8958aae182d8f48586a6e52b406f
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-03-24 11:20:01 -0600

    test for ship length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -13,6 +13,10 @@ class ShipTest < Minitest::Test
 
   def test_test_ship_name
     assert_equal "Cruiser", @cruiser.name
+  end
+
+  def test_can_retrieve_ship_length
+    assert_equal 3, @cruiser.length
 
   end
 end
