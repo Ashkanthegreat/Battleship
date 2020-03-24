7f11c0a7e914ec21093d42f811c4ba40245a000b
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-24 14:54:32 -0600

    Create fired upon and fire upon tests and methods.

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -11,8 +12,17 @@ class Cell
   end
 
   def place_ship(ship)
-    @ship = ship 
+    @ship = ship
   end
 
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+    @ship != nil
+    @ship.hit
+  end
 
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -25,6 +25,15 @@ class CellTest < Minitest::Test
   def test_it_can_place_ship
     @cell.place_ship(@cruiser)
     assert_equal @cruiser, @cell.ship
+    assert_equal false, @cell.empty?
+  end
+
+  def test_knows_when_fired_upon
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.fired_upon?
+    @cell.fire_upon
+    assert_equal 2, @cell.ship.health
+    assert_equal true, @cell.fired_upon?
   end
 
 end
