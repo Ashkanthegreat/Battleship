687388a7ec65db7b79be66abea32d49499640119
michelle <foleymichelle9@gmail.com>
2020-03-26 19:24:15 -0600

    Test each coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -80,6 +80,12 @@ class BoardTest < Minitest::Test
 
   def test_it_can_place_ship
     @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    @cell_1 = @board.cells["A1"]
+    assert_equal @cruiser, @cell_1.ship
+
+    @cell_2 = @board.cells["A2"]
+    assert_equal @cruiser, @cell_2.ship
   end
 
 
