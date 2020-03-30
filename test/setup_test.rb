83001931dc6d44a3b2730eff01492123e284fd61
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-30 13:27:06 -0600

    Create Setup attributes test and place computer ships method

---
+++ test/setup_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper.rb'
+require './lib/setup.rb'
+
+class SetupTest < Minitest::Test
+
+  def setup
+    @setup = Setup.new
+  end
+
+  def test_it_exists
+    assert_instance_of Setup, @setup
+  end
+
+  def test_if_it_stores_attributes
+    assert_equal Board, @setup.computer_board.class
+    assert_equal Ship, @setup.computer_ships.first.class
+    assert_equal Array, @setup.player_ships.class
+  end
+
+end
